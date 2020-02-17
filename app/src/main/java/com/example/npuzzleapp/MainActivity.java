73cc5dd3a7ba21bf907d547bb7c65d652b5f76e9
Ceogear <mustafaenesbilgic@gmail.com>
2020-02-17 17:40:14 +0300

    Simple button and text stuff

--- app/src/main/java/com/example/npuzzleapp/MainActivity.java
+++ app/src/main/java/com/example/npuzzleapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.npuzzleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tekst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tekst = (TextView) findViewById(R.id.textView);
+    }
+
+    public void shuffle(android.view.View v){
+        tekst.setText("shuffle");
+    }
+
+    public void reset(android.view.View v){
+        tekst.setText("reset");
     }
 }
